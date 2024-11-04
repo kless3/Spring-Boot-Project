3c1788b9eb49e56dacf144678e03eac13bc413d8
HarmyFounder <hiklesd@gmail.com>
2024-11-05 01:43:55 +0300

    update

--- src/main/java/com/harmyFounder/SpringBootProject/Application.java
+++ src/main/java/com/harmyFounder/SpringBootProject/Application.java
@@ -2,6 +2,8 @@ package com.harmyFounder.SpringBootProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class Application {
